0ba218f11a5a2d3830da9a934f46b46dc66f09d4
mura0149 <31722880+mura0149@users.noreply.github.com>
2018-03-05 12:48:23 +0100

    New

---
+++ ClassLibrary4Tests/personTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4.Tests
+{
+    [TestClass()]
+    public class personTests
+    {
+        [TestMethod]
+        public void Testmethod1 ()
+        {
+            class C = new class1();
+        var  res
+        }
+    }
+}
---
+++ ClassLibrary4Tests1/personTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4.Tests
+{
+    [TestClass()]
+    public class personTests
+    {
+        [TestMethod()]
+        public void TestNameTest()
+        {
+            person p1 = new person();
+            string name = p1.Getname();
+            Assert.AreEqual("Murat", name);        
+            
+        }
+    }
+}
